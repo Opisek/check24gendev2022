91031a750afdfa39706b38c9af7036f9f79ad558
Opisek <darowski.kacper@gmail.com>
2022-10-19 12:48:40 +0200

    database setup

--- data.js
+++ data.js
@@ -14,7 +14,31 @@ async function initializeDatabase() {
 
 async function main() {
     let connection = await pool.getConnection();
-    //await connection.query("CREATE TABLE IF NOT EXISTS hotels (id int primary key DEFAULT, name, latitude, longitude, category_stars)"); // proper table definiton to be added
+    await connection.query("CREATE TABLE IF NOT EXISTS hotels(\
+        id INT PRIMARY KEY DEFAULT,\
+        name VARCHAR,\
+        latitude DOUBLE,\
+        longitude DOUBLE,\
+        category_stars TINYINT\
+    )");
+    await connection.query("CREATE TABLE IF NOT EXISTS offers (\
+        id INT PRIMARY KEY DEFAULT,\
+        hotelid INT,\
+        departuredate DATETIME,\
+        returndate DATETIME,\
+        countadults TINYINT,\
+        countchildren TINYINT,\
+        price SMALLINT,\
+        inbounddepartureairport CHAR(3),\
+        inboundarrivalairport CHAR(3),\
+        inboundarrivaldatetime DATETIME,\
+        outbounddepartureairport CHAR(3),\
+        outboundarrivalairport CHAR(3),\
+        outboundarrivaldatetime DATETIME,\
+        mealtype VARCHAR\
+        oceanview BOOL\
+        roomtype VARCHAR\
+    )");
 }
 
 main();
