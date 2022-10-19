9b6362ae5bdf433756b7ec1fa9c8211258fb564a
Opisek <darowski.kacper@gmail.com>
2022-10-19 09:04:47 +0200

    proxy-based automatic hostname recognition

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ const path = require("path");
 const http = require("http");
 require("dotenv").config();
 
-const useHttps = true;
-const host = "test.opisek.ddnsgeek.com";
-const localPort = 8084;
-const publicPort = 443;
-
-const httpProtocol = useHttps ? "https" : "http";
-const socketProtocol = useHttps ? "wss" : "ws";
-
 // Web Server
 const express = require("express");
 const bodyParser = require('body-parser')
@@ -31,17 +23,19 @@ server.set("/partials", path.join(publicPath + '/partials'))
 server.use("/css", express.static(path.join(publicPath + '/css')));
 server.use("/js", express.static(path.join(publicPath + '/js')));
 
+server.set("trust proxy", "loopback, linklocal, uniquelocal");
+
 server.get("/", function(req, res) {
     //if (authenticate(req, res)) return;
 
-    res.render(`index`, {host: `https://${host}`});
+    res.render(`index`, { host: `${req.protocol}://${req.hostname}` });
     res.end();
 });
 
 server.get("/search/", function(req, res) {
     //if (authenticate(req, res)) return;
 
-    res.render(`search`, {host: `https://${host}`});
+    res.render(`search`, {host: `${req.protocol}://${req.hostname}` });
     res.end();
 });
 
