dcafb7c2eae184270cf6544ded6594e66af286f1
Opisek <darowski.kacper@gmail.com>
2022-10-22 23:06:04 +0200

    switched to a more developer-friendly postgresql interface library

--- data.js
+++ data.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
-const postgress = require("postgres");
+
+const postgress = require("pg");
+
 const moment = require("moment-timezone");
 
 async function main() {
-    const sql = postgress({
+    const sql = new postgress.Client({
         host: process.env.DB_HOST,
         port: process.env.DB_PORT, 
-        username: process.env.DB_USER,
+        user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE
     });
-    await sql`SET client_encoding='UTF8'`;
+    await sql.connect();
+
+    console.log("set utf8");
+    await sql.query("SET client_encoding='UTF8'");
 
     /*let cases = "";
     for (let i = 0; i < 20; ++i) {
@@ -20,14 +25,14 @@ async function main() {
     console.log(query);*/
     
     console.log("resetting");
-    await sql`DROP TABLE IF EXISTS offers, hotels`;
+    await sql.query("DROP TABLE IF EXISTS offers, hotels");
 
     console.log("create hotels");
-    await sql`CREATE TABLE IF NOT EXISTS hotels (id INT PRIMARY KEY, name VARCHAR(256), latitude NUMERIC(12, 10), longitude NUMERIC(12, 10), category_stars NUMERIC(2, 1))`;
+    await sql.query("CREATE TABLE IF NOT EXISTS hotels (id INT PRIMARY KEY, name VARCHAR(256), latitude NUMERIC(12, 10), longitude NUMERIC(12, 10), category_stars NUMERIC(2, 1))");
 
     console.log("create offers");
     const maxAdultsCount = 10;
-    await sql`
+    await sql.query(`
 CREATE TABLE IF NOT EXISTS offers\
 (\
 id SERIAL,\
@@ -48,31 +53,28 @@ outboundarrivaldatetime TIMESTAMP,\
 mealtype VARCHAR(64),\
 oceanview BOOL,\
 roomtype VARCHAR(64)\
-)`//\
-//PARTITION BY LIST(countadults)`;
+) PARTITION BY LIST(countadults)`);
+
 // id should technically be PRIMARY KEY but that is not possible due no partitions
 // CONSTRAINT hotelreference FOREIGN KEY(hotelid) REFERENCES hotels(id), // not supported by partitions
 
     console.log("partition offers");
     for (let i = 0; i <= maxAdultsCount; ++i) {
         console.log(`partition ${i}`);
-        //sql`CREATE TABLE offers${i} PARTITION OF offers FOR VALUES IN (${i})`;
+        sql.query(`CREATE TABLE ${`offers_${i}`} PARTITION OF offers FOR VALUES IN (${i})`);
     }
-    
-    console.log("set utf8");
-    await sql`SET client_encoding='UTF8'`;
 
     console.log("load hotels");
     //await sql`COPY hotels FROM '${process.env.DB_CSV}/hotels.csv' DELIMITER ',' CSV HEADER`;
-    await sql`COPY hotels FROM 'D:/Projects/Websites/check24gendev2022/csv/hotels.csv' DELIMITER ',' CSV HEADER`;
+    await sql.query(`COPY hotels FROM 'D:/Projects/Websites/check24gendev2022/csv/hotels.csv' DELIMITER ',' CSV HEADER`);
 
     console.log("load offers");
     let done = false;
-    sql`COPY offers (hotelid, departuredate, returndate, countadults, countchildren, price, inbounddepartureairport, inboundarrivalairport, inboundairline, inboundarrivaldatetime, outbounddepartureairport, outboundarrivalairport, outboundairline, outboundarrivaldatetime, mealtype, oceanview, roomtype) FROM 'D:/Projects/Websites/check24gendev2022/csv/offers.csv' DELIMITER ',' CSV HEADER`.then(() => done = true);
+    sql.query(`COPY offers (hotelid, departuredate, returndate, countadults, countchildren, price, inbounddepartureairport, inboundarrivalairport, inboundairline, inboundarrivaldatetime, outbounddepartureairport, outboundarrivalairport, outboundairline, outboundarrivaldatetime, mealtype, oceanview, roomtype) FROM 'D:/Projects/Websites/check24gendev2022/csv/offers.csv' DELIMITER ',' CSV HEADER`).then(() => done = true);
     let differences = [];
     let lastProgress = 0;
     while (!done) {
-        let progress = await sql`SELECT bytes_processed, bytes_total FROM pg_stat_progress_copy`;
+        let progress = await sql.query("SELECT bytes_processed, bytes_total FROM pg_stat_progress_copy");
         let currentProgress = progress[0].bytes_processed / progress[0].bytes_total;
         differences.push(currentProgress - lastProgress);
         if (differences.length > 20) differences.shift();
