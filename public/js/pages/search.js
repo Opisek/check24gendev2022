e34b92f958a43001ee8c43e66e9822ad5c3eae11
Opisek <darowski.kacper@gmail.com>
2022-10-24 20:37:21 +0200

    url filter parameters

--- public/js/pages/search.js
+++ public/js/pages/search.js
@@ -1,15 +1,18 @@
-function validateValue(input, change=0) {
+function setUrlParameter(paramater, value) {
+    let url = new URL(window.location.href);
+    url.searchParams.set(paramater, value);
+    history.replaceState({}, "Title", url.toString());
+}
+
+function changeValue(input, change=0, apply=true) {
     let currentValue = parseInt(input.value);
     if (isNaN(currentValue)) currentValue = 0;
     currentValue += change;
     if (currentValue < input.min) currentValue = input.min;
     else if (currentValue > input.max) currentValue = input.max;
     input.value = currentValue;
-}
-
-function changeValue(input, change=0) {
-    validateValue(input, change);    
     changeInputWidth(input);
+    if (apply) setUrlParameter(input.name, input.value);
 }
 
 function changeInputWidth(input) {
@@ -23,36 +26,37 @@ const priceRounding = 100;
 
 function upperSliderChange () {
     priceElements.upperField.value = priceElements.upperSlider.value * priceRounding;
-    upperFieldChange();
+    upperFieldChange(true);
 }
 function lowerSliderChange () {
     priceElements.lowerField.value = priceElements.lowerSlider.value * priceRounding;
-    lowerFieldChange();
+    lowerFieldChange(true);
 }
-function upperFieldChange () {
-    changeValue(priceElements.upperField);
+function upperFieldChange (sliderChange = false) {
+    changeValue(priceElements.upperField, 0, false);
     priceElements.upperSlider.value = Math.ceil(priceElements.upperField.value / priceRounding);
     priceElements.lowerField.max = priceElements.upperField.value;
-    changeValue(priceElements.lowerField);
+    changeValue(priceElements.lowerField, 0, false);
     priceElements.lowerSlider.value = Math.floor(priceElements.lowerField.value / priceRounding);
+    if (!sliderChange) {
+        setUrlParameter(priceElements.upperField.name, priceElements.upperField.value);
+        setUrlParameter(priceElements.lowerField.name, priceElements.lowerField.value);
+    }
 }
-function lowerFieldChange () {
-    changeValue(priceElements.lowerField);
+function lowerFieldChange (sliderChange = false) {
+    changeValue(priceElements.lowerField, 0, false);
     priceElements.lowerSlider.value = Math.floor(priceElements.lowerField.value / priceRounding);
     priceElements.upperField.min = priceElements.lowerField.value;
-    changeValue(priceElements.upperField);
+    changeValue(priceElements.upperField, 0, false);
     priceElements.upperSlider.value = Math.ceil(priceElements.upperField.value / priceRounding);
+    if (!sliderChange) {
+        setUrlParameter(priceElements.upperField.name, priceElements.upperField.value);
+        setUrlParameter(priceElements.lowerField.name, priceElements.lowerField.value);
+    }
 }
 
 window.addEventListener("load", () => {
-    for (let input of document.getElementsByClassName("filterInput")) {
-        if (input.type != "number") continue;
-        console.log("found");
-        input.addEventListener("input", () => changeInputWidth(input));
-        input.addEventListener("change", () => changeValue(input));
-        for (let button of input.parentElement.getElementsByClassName("filterButton")) button.addEventListener("click", () => changeValue(input, parseInt(button.value)))
-        changeValue(input);
-    }
+    const url = new URL(window.location.href);
 
     priceElements.upperSlider = document.getElementById("filterInputPriceUpperSlider");
     priceElements.lowerSlider = document.getElementById("filterInputPriceLowerSlider");
@@ -66,14 +70,32 @@ window.addEventListener("load", () => {
     priceElements.lowerSlider.min = 0;
     priceElements.upperField.min = 0;
     priceElements.lowerField.min = 0;
-    priceElements.upperSlider.value = Math.ceil(maxPrice / priceRounding);
-    priceElements.lowerSlider.value = 0;
     priceElements.upperField.value = maxPrice;
     priceElements.lowerField.value = 0;
+
+    for (let input of document.getElementsByClassName("filterInput")) {
+        if (input.type == "number") {
+            input.addEventListener("input", () => changeInputWidth(input));
+            input.addEventListener("change", () => changeValue(input));
+            for (let button of input.parentElement.getElementsByClassName("filterButton")) button.addEventListener("click", () => changeValue(input, parseInt(button.value)))
+        }
+        // todo: add search bar query from and to url
+        if (input.name != undefined) {
+            input.value = url.searchParams.get(input.name);
+        }
+        if (input.type != "range") {
+            changeValue(input, 0, false);
+        }
+    }
+
+    priceElements.upperSlider.value = Math.ceil(priceElements.upperField.value / priceRounding);
+    priceElements.lowerSlider.value = Math.floor(priceElements.lowerField.value / priceRounding);
     changeInputWidth(priceElements.upperField);
     changeInputWidth(priceElements.lowerField);
     priceElements.upperSlider.addEventListener("input", () => upperSliderChange());
     priceElements.lowerSlider.addEventListener("input", () => lowerSliderChange());
+    priceElements.upperSlider.addEventListener("change", () => changeValue(priceElements.upperField));
+    priceElements.lowerSlider.addEventListener("change", () => changeValue(priceElements.lowerField));
     priceElements.upperField.addEventListener("change", () => upperFieldChange());
     priceElements.lowerField.addEventListener("change", () => lowerFieldChange());
 });
