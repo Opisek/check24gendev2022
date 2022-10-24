d1ee17f5ed5458e266780b65f2ed65cfcfc4f9bf
Opisek <darowski.kacper@gmail.com>
2022-10-24 09:17:10 +0200

    filter buttons functionality

--- public/js/pages/search.js
+++ public/js/pages/search.js
@@ -1,3 +1,12 @@
+function changeValue(input, change) {
+    let currentValue = parseInt(input.value);
+    if (isNaN(currentValue)) currentValue = 0;
+    currentValue += change;
+    if (currentValue < 0) currentValue = 0;
+    input.value = currentValue;
+    changeInputWidth(input);
+}
+
 function changeInputWidth(input) {
     if (input.value.length > 5) input.value = input.value.substring(0, 5);
     input.style.width = `${2+.4*input.value.length}em`;
@@ -8,5 +17,8 @@ window.addEventListener("load", () => {
         input.addEventListener("keyup", () => changeInputWidth(input));
         input.addEventListener("keydown", () => changeInputWidth(input));
         input.addEventListener("input", () => changeInputWidth(input));
+        for (let button of input.parentElement.getElementsByClassName("filterButton")) {
+            button.addEventListener("click", () => changeValue(input, parseInt(button.value)))
+        }
     }
 });
