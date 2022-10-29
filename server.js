87a27f5e87f9337a7dfb3cdc419cc3f0407b3492
Opisek <darowski.kacper@gmail.com>
2022-10-29 18:23:23 +0200

    fragmented some code into separate files

--- server.js
+++ server.js
@@ -1,59 +1,8 @@
-// Constants
-
-const fs = require("fs");
-const path = require("path");
-const http = require("http");
-const socketio = require("socket.io");
 require("dotenv").config();
 
-// Web Server
-const express = require("express");
-const bodyParser = require('body-parser')
-const server = express();
-server.use(express.json());
-server.use(express.urlencoded({extended:false}));
-server.set("view engine", "ejs");
-
-const urlParser = require("parseurl");
-const cookieParser = require("cookie-parser");
-
-const serverPath = __dirname;
-const publicPath = path.join(serverPath + '/public');
-server.set("views", path.join(publicPath + '/ejs'))
-server.set("/partials", path.join(publicPath + '/partials'))
-server.use("/css", express.static(path.join(publicPath + '/css')));
-server.use("/js", express.static(path.join(publicPath + '/js')));
-
-server.set("trust proxy", "loopback, linklocal, uniquelocal");
-
-const httpServer = http.createServer(server);
-
-server.get("/", function(req, res) {
-    //if (authenticate(req, res)) return;
-
-    res.render(`index`, { host: `${req.protocol}://${req.hostname}` });
-    res.end();
-});
-
-server.get("/search/", function(req, res) {
-    //if (authenticate(req, res)) return;
-
-    res.render(`search`, {host: `${req.protocol}://${req.hostname}` });
-    res.end();
-});
-
-socketio(httpServer).on("connection", socket => {
-    console.log("client connected");
-
-    socket.on("authenticate", token => {
-        // not yet supported
-    });
-
-    socket.on("requestOffers", filters => {
-        console.log(`offers requested:\n${JSON.stringify(filters)}`);
-    });
-});
-
-
-httpServer.listen(process.env.WEB_PORT);
-console.log("listening on " + process.env.WEB_PORT);
+const webServer = new (require("./private/js/webserver"))(process.env.WEB_PORT);
+const database = new (require("./private/js/database"))(process.env.DB_HOST, process.env.DB_PORT, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_DATABASE);
+(async () => {
+    await database.connect();
+    console.log("database connected!")
+})();
